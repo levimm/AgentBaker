bead9ddebe6a0a4e6ece084d51be41a6c3357b05
Li Ma <lima2@microsoft.com>
2020-07-14 22:28:49 +0800

    vendor 0.0.111

--- parts/linux/cloud-init/artifacts/reconcile-private-hosts.sh
+++ parts/linux/cloud-init/artifacts/reconcile-private-hosts.sh
@@ -4,11 +4,6 @@ set -o nounset
 set -o pipefail
 SLEEP_SECONDS=15
 clusterFQDN="{{FQDN}}"
-echo "clusterFQDN: $clusterFQDN"
-clusterFQDN1="{{kubernetesEndpoint}}"
-echo "clusterFQDN1: $clusterFQDN1"
-clusterFQDN2="{{API_SERVER_NAME}}"
-echo "clusterFQDN2: $clusterFQDN2"
 
 if [[ $clusterFQDN != *.privatelink.* ]]; then
   echo "skip reconcile hosts for $clusterFQDN since it's not AKS private cluster"
